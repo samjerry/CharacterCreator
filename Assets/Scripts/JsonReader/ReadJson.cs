a0f8101956e2b1ded6d03411585b299b25b2e989
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-27 22:55:57 +0200

    Added 3 UI Text fields in the JsonReaderScene, Added DetailsToText function in ReadJson.cs, Removed the 3 smaller Json files from the project, Removed the JsonHelper.cs

--- Assets/Scripts/JsonReader/ReadJson.cs
+++ Assets/Scripts/JsonReader/ReadJson.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadJson : MonoBehaviour
 {
     [SerializeField] private TextAsset _charNameFile;
     [SerializeField] private TextAsset _charDetailsFile;
 
-    //[SerializeField] private TextAsset _charRacesFile;
-   // [SerializeField] private TextAsset _charClassesFile;
-   // [SerializeField] private TextAsset _charStatsFile;
-
+    [SerializeField] private Text _raceText;
+    [SerializeField] private Text _classText;
+    [SerializeField] private Text _statText;
 
     void Start()
     {
         Character _myChar = new Character(); 
         _myChar = JsonUtility.FromJson<Character>(_charDetailsFile.text);
 
-        DebugArray(_myChar.charRaces);
-        DebugArray(_myChar.charClasses);
-        DebugArray(_myChar.charStats);
+        _statText.text = "";
+        _classText.text = "";
+        _raceText.text = "";
+
+        DetailsToText(_myChar);
+
+        //DebugArray(_myChar.charRaces);
+        //DebugArray(_myChar.charClasses);
+        //DebugArray(_myChar.charStats);
+    }
+
+    private void DetailsToText(Character _myChar)
+    {
+        for (int i = 0; i < _myChar.charRaces.Length; i++)
+        {
+            if (i == _myChar.charRaces.Length)
+            {
+                _raceText.text += _myChar.charRaces[i];
+                break;
+            }
+            _raceText.text += _myChar.charRaces[i] + ",\n";
+        }
+
+        for (int j = 0; j < _myChar.charClasses.Length; j++)
+        {
+            if (j == _myChar.charClasses.Length)
+            {
+                _classText.text += _myChar.charClasses[j];
+                break;
+            }
+            _classText.text += _myChar.charClasses[j] + "\n";
+        }
+
+        for (int g = 0; g < _myChar.charStats.Length; g++)
+        {
+            if (g == _myChar.charStats.Length)
+            {
+                _statText.text += _myChar.charStats[g];
+                break;
+            }
+            _statText.text += _myChar.charStats[g] + ",\n";
+        }
     }
 
     private void DebugArray(string[] _array)
