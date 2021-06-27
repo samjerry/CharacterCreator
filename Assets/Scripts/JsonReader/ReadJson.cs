2a33f3cd12a04e60247d87cf364b2497ff3f9cce
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-27 21:02:43 +0200

    Removed the 2D-Array logger from ReadJson.cs

--- Assets/Scripts/JsonReader/ReadJson.cs
+++ Assets/Scripts/JsonReader/ReadJson.cs
@@ -9,17 +9,6 @@ public class ReadJson : MonoBehaviour
 
     void Start()
     {
-        Character _myChar = new Character();
-        _myChar.charClasses = JsonUtility.FromJson<Character>(_charDetailsFile.text).charClasses;
-        //_myChar.charNames = JsonUtility.FromJson<Character>(_charNameFile.text);
 
-
-        for (int i = 0; i < _myChar.charNames.GetLength(0); i++)
-        {
-            for (int j = 0; j < _myChar.charNames.GetLength(1); j++)
-            {
-                Debug.Log(_myChar.charNames[i,j]);
-            }
-        }
     }
 }
