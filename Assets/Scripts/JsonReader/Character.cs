c7dcc9c635c327bb0a6a179c6e9efa6e6e3f7683
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-27 18:51:12 +0200

    Added two JsonUtility.FromJson to ReadJson.cs

--- Assets/Scripts/JsonReader/Character.cs
+++ Assets/Scripts/JsonReader/Character.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [System.Serializable]
 public class Character : MonoBehaviour
 {
-    public string[] charNames;
+    public string[,] charNames;
     public string[] charRaces;
     public string[] charClasses;
+    public string[] charStats;
 
     public string charName;
     public string charRace;
     public string charClass;
+    public string charStatPrio;
 }
--- Assets/Scripts/JsonReader/ReadJson.cs
+++ Assets/Scripts/JsonReader/ReadJson.cs
@@ -6,5 +6,17 @@ public class ReadJson : MonoBehaviour
 {
     [SerializeField] private TextAsset _charNameFile;
     [SerializeField] private TextAsset _charDetailsFile;
-    
+
+    void Start()
+    {
+        Character _myChar = new Character();
+        _myChar = JsonUtility.FromJson<Character>(_charDetailsFile.text);
+        _myChar = JsonUtility.FromJson<Character>(_charNameFile.text);
+
+
+        foreach (Character character in _myChar.charNames)
+        {
+            Debug.Log("Found employee: " + _myChar.charNames);
+        }
+    }
 }
