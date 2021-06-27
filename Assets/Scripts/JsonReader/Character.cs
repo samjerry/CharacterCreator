42af9624ff18d7702c45b0c41a8c68bc1540ebef
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-27 18:20:09 +0200

    Set Character.cs to be Serializable

--- Assets/Scripts/JsonReader/Character.cs
+++ Assets/Scripts/JsonReader/Character.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Character : MonoBehaviour
 {
     // Start is called before the first frame update
