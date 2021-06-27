4e2236f64b3d2c5387fb42c8e4d1be85ae7aca64
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-27 18:25:11 +0200

    Added the base name/race/class variables to Character.cs

--- Assets/Scripts/JsonReader/Character.cs
+++ Assets/Scripts/JsonReader/Character.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    string charName;
+    string charRace;
+    string charClass;
 }
