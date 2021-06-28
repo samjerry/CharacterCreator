2797339f90e68bd51d037722a118f0993898d45a
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-28 02:10:48 +0200

    Added a GetRaceNames function to ReadJson.cs

--- Assets/Scripts/JsonReader/Character.cs
+++ Assets/Scripts/JsonReader/Character.cs
@@ -5,8 +5,51 @@ using UnityEngine;
 [System.Serializable]
 public class Character
 {
-    public string[][] charNames;
+    public Names charNames;
     public string[] charRaces;
     public string[] charClasses;
     public string[] charStats;
+}
+
+[System.Serializable]
+public class Names
+{
+    public string[] Aaracockra; 
+    public string[] Aasimar; 
+    public string[] Bugbear; 
+    public string[] Centaur; 
+    public string[] Changeling; 
+    public string[] Dragonborn; 
+    public string[] Dwarf; 
+    public string[] Elf; 
+    public string[] Firbolg; 
+    public string[] Genasi; 
+    public string[] Gith;
+    public string[] Gnome;
+    public string[] Goblin;
+    public string[] Goliath;
+    public string[] Half_Elf;
+    public string[] Half_Orc;
+    public string[] Halfling;
+    public string[] Hobgoblin;
+    public string[] Human;
+    public string[] Kalashtar;
+    public string[] Kenku;
+    public string[] Kobold;
+    public string[] Lizardfolk;
+    public string[] Locathah;
+    public string[] Loxodon;
+    public string[] Minotaur;
+    public string[] Orc;
+    public string[] Pureblood;
+    public string[] Shifter;
+    public string[] Simic_Hybrid;
+    public string[] Tabaxi;
+    public string[] Tiefling;
+    public string[] Tortle;
+    public string[] Triton;
+    public string[] Vedalken;
+    public string[] Verdan;
+    public string[] Warforged;
+    public string[] Yuan_ti;
 }
--- Assets/Scripts/JsonReader/ReadJson.cs
+++ Assets/Scripts/JsonReader/ReadJson.cs
@@ -12,16 +12,25 @@ public class ReadJson : MonoBehaviour
     [SerializeField] private Text _classText;
     [SerializeField] private Text _statText;
 
+    [SerializeField] public string charRace;
     void Start()
     {
-        Character _myChar = new Character(); 
+        Character _myChar = new Character();
         _myChar = JsonUtility.FromJson<Character>(_charDetailsFile.text);
+        _myChar.charNames = JsonUtility.FromJson<Names>(_charNameFile.text);
+
+        
 
         _statText.text = "";
         _classText.text = "";
         _raceText.text = "";
 
         DetailsToText(_myChar);
+        
+        foreach (var item in GetRaceNames(_myChar, charRace))
+        {
+            Debug.Log(item);
+        }
 
     }
 
@@ -56,6 +65,54 @@ public class ReadJson : MonoBehaviour
             }
             _statText.text += _myChar.charStats[i] + "\n";
         }
+    }
+
+    private string[] GetRaceNames(Character _myChar, string _race)
+    {
+        string[] _names;
+
+        switch (_race)
+        {
+            case "Aaracockra": _names = _myChar.charNames.Aaracockra; break;
+            case "Aasimar": _names = _myChar.charNames.Aasimar; break;
+            case "Bugbear": _names = _myChar.charNames.Bugbear; break;
+            case "Centaur": _names = _myChar.charNames.Centaur; break;
+            case "Changeling": _names = _myChar.charNames.Changeling; break;
+            case "Dragonborn": _names = _myChar.charNames.Dragonborn; break;
+            case "Dwarf": _names = _myChar.charNames.Dwarf; break;
+            case "Elf": _names = _myChar.charNames.Elf; break;
+            case "Firbolg": _names = _myChar.charNames.Firbolg; break;
+            case "Genasi": _names = _myChar.charNames.Genasi; break;
+            case "Gith": _names = _myChar.charNames.Gith; break;
+            case "Gnome": _names = _myChar.charNames.Gnome; break;
+            case "Goblin": _names = _myChar.charNames.Goblin; break;
+            case "Goliath": _names = _myChar.charNames.Goliath; break;
+            case "Half-Elf": _names = _myChar.charNames.Half_Elf; break;
+            case "Half-Orc": _names = _myChar.charNames.Half_Orc; break;
+            case "Halfling": _names = _myChar.charNames.Halfling; break;
+            case "Hobgoblin": _names = _myChar.charNames.Hobgoblin; break;
+            case "Kalashtar": _names = _myChar.charNames.Kalashtar; break;
+            case "Kenku": _names = _myChar.charNames.Kenku; break;
+            case "Kobold": _names = _myChar.charNames.Kobold; break;
+            case "Lizardfolk": _names = _myChar.charNames.Lizardfolk; break;
+            case "Locathah": _names = _myChar.charNames.Locathah; break;
+            case "Loxodon": _names = _myChar.charNames.Loxodon; break;
+            case "Minotaur": _names = _myChar.charNames.Minotaur; break;
+            case "Orc": _names = _myChar.charNames.Orc; break;
+            case "Pureblood": _names = _myChar.charNames.Pureblood; break;
+            case "Shifter": _names = _myChar.charNames.Shifter; break;
+            case "Simic-Hybrid": _names = _myChar.charNames.Simic_Hybrid; break;
+            case "Tabaxi": _names = _myChar.charNames.Tabaxi; break;
+            case "Tiefling": _names = _myChar.charNames.Tiefling; break;
+            case "Tortle": _names = _myChar.charNames.Tortle; break;
+            case "Triton": _names = _myChar.charNames.Triton; break;
+            case "Vedalken": _names = _myChar.charNames.Vedalken; break;
+            case "Verdan": _names = _myChar.charNames.Verdan; break;
+            case "Warforged": _names = _myChar.charNames.Warforged; break;
+            case "Yuan-ti": _names = _myChar.charNames.Yuan_ti; break;
+            default: _names = null; break;
+        }
+        return _names;
     }
 }
 
