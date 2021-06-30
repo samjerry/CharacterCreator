613b3090bd183f19b36e2beec700d28aedbd44a9
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-06-30 21:33:24 +0200

    Added _storyTextField variable, GenerateBackstory function and RandomStringFromArray function to BackstoryGenerator.cs

--- Assets/Scripts/Feature Backstory/BackstoryGenerator.cs
+++ Assets/Scripts/Feature Backstory/BackstoryGenerator.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackstoryGenerator : MonoBehaviour
 {
+    [SerializeField] private Text _storyTextField;
+    private void Start()
+    {
+        _storyTextField.text = GenerateBackstory();
+    }
+
     public string GenerateBackstory() {
-        string _backstory = "";
+        string _backstory = "adjective, race, class from location, who, details";
+        string _adjective = "adjective";
+        string _race = "race";
+        string _class = "class";
+        string _location = "location";
+        string _details = "details";
 
-        GameObject _jsonReader = GameObject.Find("JsonReader");
+        Character _newChar = GameObject.Find("JsonReader").GetComponent<ReadJson>().newChar;
 
+        _adjective = RandomStringFromArray(_newChar.storyAdjective);
+        _location = RandomStringFromArray(_newChar.storyLocation);
+        _details = RandomStringFromArray(_newChar.storyDetails);
+        _race = RandomStringFromArray(_newChar.charRaces);
+        _class = RandomStringFromArray(_newChar.charClasses);
 
+        _backstory = _adjective + " " + _race + " " + _class + " from " + _location + " who " + _details;
         return _backstory;
     }
+
+    private string RandomStringFromArray(string[] strArray) => strArray[Random.Range(0, strArray.Length)]; 
 }
