a6b3db185d83b22aa16f823957fc2027421167a7
Michelle <Minoord18@outlook.com>
2021-07-04 18:34:21 +0200

    Added RaceGenerator.cs

---
+++ Assets/Scripts/Feature Race/RaceGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceGenerator : MonoBehaviour
+{
+    // Variables used to get the variable charRace from ReadJson
+    [SerializeField] private ReadJson _readJson;
+    [SerializeField] private string setCharRace;
+
+
+    //Function to randomize the race
+    public string RandomizeRace()
+    {
+
+        return;
+    }
+
+
+}
