296d9c43b18a22a4217957cc7b412cea7776179d
Michelle <Minoord18@outlook.com>
2021-07-05 13:29:20 +0200

    Changed RaceGenerator, Function RandomizeRace moved to line 37, Moved the for loop from function RandomizeRace to the void update, Added the string _readJson.newChar.charRaces to the for loop.

--- Assets/Scripts/Feature Race/RaceGenerator.cs
+++ Assets/Scripts/Feature Race/RaceGenerator.cs
@@ -6,40 +6,39 @@ public class RaceGenerator : MonoBehaviour
 {
     // Variables used to get the variable charRace from ReadJson
     [SerializeField] private ReadJson _readJson;
-    [SerializeField] private string setCharRace;
 
     // The Index Number that is going to be randomized 
     [SerializeField] private int IndexNum; 
 
-    //Function to randomize the race
-    public int RandomizeRace()
+    void Start()
     {
-        // Randomized the Index Number
-        IndexNum = Random.Range(0, 38);
+        RandomizeRace();
+    }
 
+    void Update()
+    {
         //Goes by every Race 
-        for (int i = 0; i < 38; i++)
+        for (int i = 0; i < _readJson.newChar.charRaces.Length; i++)
         {
-            if(i == IndexNum)
+            if (i == IndexNum)
             {
-                
+                _readJson.charRace = _readJson.newChar.charRaces[i];
             }
             else
             {
                 i += 1;
             }
         }
+        
+        print(_readJson.charRace);
+
+    }
+    //Function to randomize a number, so that it later can use it to know which Index number it should take
+    int RandomizeRace()
+    {
+        // Randomized the Index Number
+        IndexNum = Random.Range(0, 38);
         return IndexNum;
     }
-
-    public void Start()
-    {
-        RandomizeRace();
-    }
-    private void Update()
-    {
-        print(IndexNum);
-    }
-
 
 }
