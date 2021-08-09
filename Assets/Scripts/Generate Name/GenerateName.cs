912adb4ed506196b88ad4622e1ca8732b221e7ab
Scott van den Broek <you@example.com>
2021-08-09 13:44:10 +0200

    Added new branch

---
+++ Assets/Scripts/Generate Name/GenerateName.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateName : MonoBehaviour
+{
+    public Character character;
+    void GetRace()
+    {
+
+    }
+}
